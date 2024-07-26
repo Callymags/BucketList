b4809456b1bbd71e5bc42618bdfde3790b8cdd16
Callan John Maguire <callanjohn.maguire@version1.com>
2024-07-26 13:38:25 +0100

    Initial commit

---
+++ src/main/java/com/bucketlist/project/SbBucketlistApplication.java
@@ -0,0 +1,13 @@
+package com.bucketlist.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBucketlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBucketlistApplication.class, args);
+	}
+
+}
