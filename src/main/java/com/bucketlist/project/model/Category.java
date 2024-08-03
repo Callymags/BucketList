2b4e0587a7adff0c56630ab980bb66f178b47086
Callan John Maguire <callanjohn.maguire@version1.com>
2024-08-03 21:22:45 +0100

    Connect Application to Database.
    - Added jpa dependency in pom.
    - Added Category Repository.
    - Updated CategoryServiceImpl CRUD operations.
    - Added dotenv dependency for use of .env file

--- src/main/java/com/bucketlist/project/model/Category.java
+++ src/main/java/com/bucketlist/project/model/Category.java
@@ -1,9 +1,18 @@
 package com.bucketlist.project.model;
 
+import javax.persistence.*;
+
+@Entity (name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
+    @SequenceGenerator(name = "category_seq", sequenceName = "category_seq", allocationSize = 1)
     private Long categoryId;
 
     private String CategoryName;
+
+    public Category() {
+    }
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
---
+++ src/main/java/com/bucketlist/project/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.bucketlist.project.repositories;
+
+import com.bucketlist.project.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
